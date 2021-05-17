113991d4f27137defca068c29cde1d1ad3331304
Samvel <semik.perikhanyan@gmail.com>
2021-05-17 23:20:47 +0400

    Методы и свойства строк и чисел

---
+++ work17/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const str = "teSt";
+
+// console.log(str[2] = 'd');
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit")); // С какой позиции начинается... -1 - когда не найдено
+
+
+const logg = "Hello world";
+// console.log(logg.slice(6, 11));
+console.log(logg.slice(-5, -1));
+
+
