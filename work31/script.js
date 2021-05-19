3ca654e4a83f8e134456ec1aa18c98ed5c9170d3
semik <semik.perikhanyan@gmail.com>
2021-05-20 00:05:01 +0400

    События и их обработчики

---
+++ work31/script.js
@@ -0,0 +1,35 @@
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
+// btn.onclick = function() {
+//     alert ('Click');
+// };
+
+// btn.onclick = function() {
+//     alert ('Second Click');
+// };
+
+let i = 0;
+const deleteElement = (e) => {
+    console.log(e.target);
+    console.log(e.type);
+
+    // i++;
+    // if(i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    console.log(e.target);
+});
